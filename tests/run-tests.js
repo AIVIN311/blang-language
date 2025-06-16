881cd39c55caaf6e149d23f65471b34926b87d8c
Eastily <wayne_minn@icloud.com>
2025-06-16 21:59:17 +1000

    Fix selector quoting and add tests

--- tests/run-tests.js
+++ tests/run-tests.js
@@ -219,6 +219,52 @@ function testShowImageParsing() {
   }
 }
 
+function testAddElementParsing() {
+  const sample = '新增元素 div 到 #容器';
+  const originalDemo = fs.readFileSync('demo.blang', 'utf8');
+  fs.writeFileSync('demo.blang', sample);
+
+  const hasOutput = fs.existsSync('output.js');
+  const originalOut = hasOutput ? fs.readFileSync('output.js', 'utf8') : null;
+
+  execSync('node parser_v0.9.4.js');
+  const output = fs.readFileSync('output.js', 'utf8');
+  assert(
+    output.includes('document.querySelector("#容器").appendChild(document.createElement("div"));'),
+    '新增元素 should append element to quoted selector'
+  );
+
+  fs.writeFileSync('demo.blang', originalDemo);
+  if (hasOutput) {
+    fs.writeFileSync('output.js', originalOut);
+  } else {
+    fs.unlinkSync('output.js');
+  }
+}
+
+function testFadeAnimationParsing() {
+  const sample = '增加透明度動畫到 #方塊';
+  const originalDemo = fs.readFileSync('demo.blang', 'utf8');
+  fs.writeFileSync('demo.blang', sample);
+
+  const hasOutput = fs.existsSync('output.js');
+  const originalOut = hasOutput ? fs.readFileSync('output.js', 'utf8') : null;
+
+  execSync('node parser_v0.9.4.js');
+  const output = fs.readFileSync('output.js', 'utf8');
+  assert(
+    output.includes('document.querySelector("#方塊").style.transition = \'opacity 0.5s\';'),
+    '增加透明度動畫 should apply transition with quoted selector'
+  );
+
+  fs.writeFileSync('demo.blang', originalDemo);
+  if (hasOutput) {
+    fs.writeFileSync('output.js', originalOut);
+  } else {
+    fs.unlinkSync('output.js');
+  }
+}
+
 function testContentLengthCondition() {
   const sample = '如果（字串.內容長度 > 3）：';
   const originalDemo = fs.readFileSync('demo.blang', 'utf8');
@@ -502,6 +548,8 @@ try {
   testToggleColorParsing();
   testMultipleToggleColor();
   testShowImageParsing();
+  testAddElementParsing();
+  testFadeAnimationParsing();
   testSetSelectorContent();
   testLogStatement();
   testPlayVideoParsing();
